b9df0f0ea46b8d4b9cad0ea9f18f772ab724952e
Alex <developer-six@relative-engine.com>
2020-01-09 10:07:13 -0500

    CLIENTE

--- quski-oro-rest/src/main/java/com/relative/midas/rest/ClienteRestController.java
+++ quski-oro-rest/src/main/java/com/relative/midas/rest/ClienteRestController.java
@@ -1,12 +1,19 @@
 package com.relative.midas.rest;
+
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
 import com.relative.core.exception.RelativeException;
 import com.relative.core.util.main.PaginatedListWrapper;
+import com.relative.core.util.main.PaginatedWrapper;
 import com.relative.core.web.util.BaseRestController;
 import com.relative.core.web.util.CrudRestControllerInterface;
 import com.relative.core.web.util.GenericWrapper;
@@ -14,6 +21,7 @@ import com.relative.quski.model.TbQoCliente;
 import com.relative.quski.service.QuskiOroService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @Path("/clienteRestController")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -30,31 +38,57 @@ implements CrudRestControllerInterface<TbQoCliente, GenericWrapper<TbQoCliente>>
 
 	@Inject
 	QuskiOroService qos;
-	
-
 	@Override
 	public void deleteEntity(String arg0) throws RelativeException {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
-	public GenericWrapper<TbQoCliente> getEntity(String arg0) throws RelativeException {
-		// TODO Auto-generated method stub
-		return null;
+	@GET
+	@Path("/getEntity")
+	@ApiOperation(value = "GenericWrapper<TbQoCliente>", notes = "Metodo getEntity Retorna wrapper de entidades encontradas en TbQoCliente", response = GenericWrapper.class)
+	public GenericWrapper<TbQoCliente> getEntity(@QueryParam("id") String id) throws RelativeException {
+		GenericWrapper<TbQoCliente> loc = new GenericWrapper<>();
+		TbQoCliente a = this.qos.findClienteById(Long.valueOf(id));
+		loc.setEntidad(a);
+		return loc;
 	}
-
+	
 	@Override
-	public PaginatedListWrapper<TbQoCliente> listAllEntities(String arg0, String arg1, String arg2, String arg3,
-			String arg4) throws RelativeException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	@GET
+	@Path("/listAllEntities")
+	@ApiOperation(value = "PaginatedListWrapper<TbQoCliente>", notes = "Metodo Get listAllEntities Retorna wrapper de informacion de paginacion y entidades encontradas en TbQoCliente", response = PaginatedListWrapper.class)
+	public PaginatedListWrapper<TbQoCliente> listAllEntities(@QueryParam("page") @DefaultValue("1") String page,
+			@QueryParam("pageSize") @DefaultValue("10") String pageSize,
+			@QueryParam("sortFields") @DefaultValue("id") String sortFields,
+			@QueryParam("sortDirections") @DefaultValue("asc") String sortDirections,
+			@QueryParam("isPaginated") @DefaultValue("N") String isPaginated) throws RelativeException {
+		Integer firstItem = Integer.valueOf(page) * Integer.valueOf(pageSize);
+		return findAll(
+				new PaginatedWrapper(firstItem, Integer.valueOf(pageSize), sortFields, sortDirections, isPaginated));
 
-	@Override
-	public GenericWrapper<TbQoCliente> persistEntity(GenericWrapper<TbQoCliente> arg0) throws RelativeException {
-		// TODO Auto-generated method stub
-		return null;
 	}
+	
+	private PaginatedListWrapper<TbQoCliente> findAll(PaginatedWrapper pw) throws RelativeException {
+		PaginatedListWrapper<TbQoCliente> plw = new PaginatedListWrapper<>(pw);
+		List<TbQoCliente> actions = this.qos.findAllCliente(pw);
+		if (actions != null && !actions.isEmpty()) {
+			plw.setTotalResults(this.qos.countCliente().intValue());
+			plw.setList(actions);
+		}
+		return plw;
+	}
+	
+	@Override
+	@POST
+	@Path("/persistEntity")
+	@ApiOperation(value = "GenericWrapper<TbQoCliente>", notes = "Metodo Post persistEntity Retorna GenericWrapper de informacion de paginacion y listado de entidades encontradas TbQoCliente", response = GenericWrapper.class)
+	public GenericWrapper<TbQoCliente> persistEntity(GenericWrapper<TbQoCliente> wp) throws RelativeException {
+		GenericWrapper<TbQoCliente> loc = new GenericWrapper<>();
+		loc.setEntidad(this.qos.manageCliente(wp.getEntidad()));
+		return loc;
+	}	
 
 }
+
