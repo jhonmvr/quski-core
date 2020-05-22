17230c770f8c44883dbb8d32335a7da3e7d01118
developer-four <developer-four@relative-engine.com>
2020-05-21 22:09:17 -0500

    Realizo el cambio en el tipo Object ya que no tenia la anotacion @Lob

--- quski-oro-datamodel-ejb/src/main/java/com/relative/quski/model/TbMiParametro.java
+++ quski-oro-datamodel-ejb/src/main/java/com/relative/quski/model/TbMiParametro.java
@@ -1,67 +1,64 @@
 package com.relative.quski.model;
 
 import java.io.Serializable;
-import javax.persistence.*;
-
-import com.relative.quski.enums.EstadoEnum;
-
-import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.relative.quski.enums.EstadoEnum;
 
 /**
  * The persistent class for the tb_mi_parametro database table.
  * 
  */
 @Entity
-@Table(name="tb_mi_parametro")
+@Table(name = "tb_mi_parametro")
 
 public class TbMiParametro implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@SequenceGenerator(name="TB_MI_PARAMETRO_ID_GENERATOR", sequenceName="SEQ_PARAMETRO",initialValue = 1,allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="TB_MI_PARAMETRO_ID_GENERATOR")
+	@SequenceGenerator(name = "TB_MI_PARAMETRO_ID_GENERATOR", sequenceName = "SEQ_PARAMETRO", initialValue = 1, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TB_MI_PARAMETRO_ID_GENERATOR")
 	private Long id;
-
-	private Object archivo;
-
-	@Column(name="caracteristica_dos")
-	private String caracteristicaDos;
-
-	@Column(name="caracteritica_uno")
-	private String caracteriticaUno;
+	private String nombre;
+	private String valor;
+	private String tipo;
 	@Enumerated(EnumType.STRING)
 	private EstadoEnum estado;
-
-	@Temporal(TemporalType.DATE)
-	@Column(name="fecha_creacion")
-	private Date fechaCreacion;
-
-	private String nombre;
+	@Column(name = "caracteritica_uno")
+	private String caracteriticaUno;
+	@Column(name = "caracteristica_dos")
+	private String caracteristicaDos;
 
 	private Long orden;
-
-	private String tipo;
-
-	private String valor;
+	@Lob
+	private Object archivo;
+	@Temporal(TemporalType.DATE)
+	@Column(name = "fecha_creacion")
+	private Date fechaCreacion;
 
 	public TbMiParametro() {
 	}
-
-	
 
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 
 	public Object getArchivo() {
 		return this.archivo;
@@ -87,8 +84,6 @@ public class TbMiParametro implements Serializable {
 		this.caracteriticaUno = caracteriticaUno;
 	}
 
- 
-
 	public Date getFechaCreacion() {
 		return this.fechaCreacion;
 	}
@@ -104,9 +99,6 @@ public class TbMiParametro implements Serializable {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-
-
 
 	public String getTipo() {
 		return this.tipo;
