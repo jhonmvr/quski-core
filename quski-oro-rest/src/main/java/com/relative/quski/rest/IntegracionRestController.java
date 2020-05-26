bf4fbda60db1ffdae009a9eda37c87c713d6392f
Brayan Monge <developer_six@relative_engine.com>
2020-05-25 22:48:05 -0500

    Integracion de los servicios de cliente y oferta

--- quski-oro-rest/src/main/java/com/relative/quski/rest/IntegracionRestController.java
+++ quski-oro-rest/src/main/java/com/relative/quski/rest/IntegracionRestController.java
@@ -1,35 +1,79 @@
 package com.relative.quski.rest;
 
-import javax.ws.rs.GET;
-import java.io.Serializable;
-import com.relative.integracion.calculadora.cliente.wrapper.Informacion;
-import com.relative.quski.service.IntegracionServiceImp;
-import com.relative.core.util.main.BaseWrapper;
-import javax.ws.rs.QueryParam;
-import com.relative.core.exception.RelativeException;
 import javax.inject.Inject;
-import io.swagger.annotations.ApiOperation;
-import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.relative.core.exception.RelativeException;
+import com.relative.core.util.main.BaseWrapper;
 import com.relative.core.web.util.BaseRestController;
+import com.relative.integracion.calculadora.cliente.wrapper.Informacion;
+import com.relative.integracion.calculadora.oferta.wrapper.SimularResponse;
+import com.relative.quski.service.IntegracionServiceImp;
+
+import io.swagger.annotations.ApiOperation;
 
 @Path("/integracionRestController")
 @Consumes({ "application/json" })
 @Produces({ "application/json" })
 @ApiOperation(value = "IntegracionRestController", notes = "Metodo Get que retorna la informacion del cliente mediante SOAP")
-public class IntegracionRestController extends BaseRestController
-{
-    @Inject
-    IntegracionServiceImp is;
-    
-    public IntegracionRestController() throws RelativeException {
-    }
-    
-    @GET
-    @Path("/getInformacionPersona")
-    public BaseWrapper<Informacion> getInformacionPersona(@QueryParam("tipoIdentificacion") final String tipoIdentificacion, @QueryParam("identificacion") final String identificacion, @QueryParam("tipoConsulta") final String tipoConsulta, @QueryParam("calificacion") final String calificacion) throws RelativeException {
-        final BaseWrapper<Informacion> info = (BaseWrapper<Informacion>)new BaseWrapper();
-        info.setEntidad((Informacion)this.is.getClienteInformacion(tipoIdentificacion, identificacion, tipoConsulta, calificacion));
-        return info;
-    }}
+public class IntegracionRestController extends BaseRestController {
+	@Inject
+	IntegracionServiceImp is;
+
+	public IntegracionRestController() throws RelativeException {
+	}
+
+	@GET
+	@Path("/getInformacionPersona")
+	public BaseWrapper<Informacion> getInformacionPersona(
+			@QueryParam("tipoIdentificacion") final String tipoIdentificacion,
+			@QueryParam("identificacion") final String identificacion,
+			@QueryParam("tipoConsulta") final String tipoConsulta,
+			@QueryParam("calificacion") final String calificacion) throws RelativeException {
+		final BaseWrapper<Informacion> info = (BaseWrapper<Informacion>) new BaseWrapper();
+		info.setEntidad((Informacion) this.is.getClienteInformacion(tipoIdentificacion, identificacion, tipoConsulta,
+				calificacion));
+		return info;
+	}
+
+	@GET
+	@Path("/getInformacionOferta")
+	public BaseWrapper<SimularResponse> getInformacionPersonaOferta(
+			@QueryParam("perfilRiesgo") final String perfilRiesgo,
+			@QueryParam("origenOperacion") final String origenOperacion,
+			@QueryParam("riesgoTotal") final String riesgoTotal,
+			@QueryParam("fechaNacimiento") final String fechaNacimiento,
+			@QueryParam("perfilPreferencia") final String perfilPreferencia,
+			@QueryParam("agenciaOriginacion") final String agenciaOriginacion,
+			@QueryParam("identificacionCliente") final String identificacionCliente,
+			@QueryParam("calificacionMupi") final String calificacionMupi,
+			@QueryParam("coberturaExcepcionada") final String coberturaExcepcionada,
+			@QueryParam("tipoJoya") final String tipoJoya,
+			@QueryParam("descripcion") final String descripcion,
+			@QueryParam("estadoJoya") final String estadoJoya,
+			@QueryParam("tipoOroKilataje") final String tipoOroKilataje,
+			@QueryParam("pesoGr") final String pesoGr,
+			@QueryParam("tienePiedras") final String tienePiedras,
+			@QueryParam("detallePiedras") final String detallePiedras,
+			@QueryParam("descuentoPesoPiedras") final String descuentoPesoPiedras,
+			@QueryParam("pesoNeto") final String pesoNeto,
+			@QueryParam("precioOro") final String precioOro,
+			@QueryParam("valorAplicableCredito") final String valorAplicableCredito,
+			@QueryParam("valorRealizacion") final String valorRealizacion,
+			@QueryParam("numeroPiezas") final String numeroPiezas,
+			@QueryParam("descuentoSuelda") final String descuentoSuelda) throws RelativeException {
+		final BaseWrapper<SimularResponse> oferta = (BaseWrapper<SimularResponse>) new BaseWrapper();
+		oferta.setEntidad((SimularResponse) this.is.getClienteInformacionOferta( perfilRiesgo,  origenOperacion,  riesgoTotal,
+				 fechaNacimiento,  perfilPreferencia,  agenciaOriginacion,  identificacionCliente,
+				 calificacionMupi,  coberturaExcepcionada,  tipoJoya,  descripcion,
+				 estadoJoya,  tipoOroKilataje,  pesoGr,  tienePiedras,  detallePiedras,
+				 descuentoPesoPiedras,  pesoNeto,  precioOro,  valorAplicableCredito,
+				 valorRealizacion,  numeroPiezas,  descuentoSuelda));
+		return oferta;
+	}
+
+}
--- quski-oro-service-ejb/src/main/java/com/relative/quski/service/IntegracionServiceImp.java
+++ quski-oro-service-ejb/src/main/java/com/relative/quski/service/IntegracionServiceImp.java
@@ -1,19 +1,76 @@
 package com.relative.quski.service;
 
-import com.relative.core.exception.RelativeException;
-import com.relative.integracion.calculadora.cliente.consumer.ClienteCalculadoraConsumer;
-import com.relative.integracion.calculadora.cliente.wrapper.Informacion;
 import javax.ejb.Stateless;
 
+import com.relative.core.exception.RelativeException;
+import com.relative.integracion.calculadora.cliente.consumer.ClienteCalculadoraConsumer;
+import com.relative.integracion.calculadora.cliente.consumer.ClienteConsultaOfertaConsumer;
+import com.relative.integracion.calculadora.cliente.wrapper.Informacion;
+import com.relative.integracion.calculadora.oferta.wrapper.SimularResponse;
+
 @Stateless
-public class IntegracionServiceImp
-{
-    public Informacion getClienteInformacion(final String tipoIdentificacion, final String identificacion, final String tipoConsulta, final String calificacion) throws RelativeException {
-        return ClienteCalculadoraConsumer.callCalculadoraClienteObject(tipoIdentificacion, identificacion, tipoConsulta, calificacion);
-    }
+public class IntegracionServiceImp {
+	
+	/**
+	 * Metodo que se encarga de extraer la informacion de clientes desde el servicio Quski Clientes
+	 * @param tipoIdentificacion tipo de indetifiacion, los valores a tomar son:<br>
+	 * 1. C cedula<br>
+	 * 2. R ruc
+	 * @param identificacion Identifiacion del cliente (cedula, ruc)
+	 * @param tipoConsulta Tipo de consulta a realizar:<br>
+	 * 1. CE: Consulta con excepción
+	 * 2  CC: Consulta con variables internas
+	 * 3  TD: Consulta sin variables interna
+	 * @param calificacion calificacion del cliente
+	 * @return Objeto con la informacion retornado si el cliente tiene datos, o el error generado.
+	 * @author BRAYAN MONGE - Relative Engine
+	 * @throws RelativeException
+	 */
+
+	public Informacion getClienteInformacion(final String tipoIdentificacion, final String identificacion,
+			final String tipoConsulta, final String calificacion) throws RelativeException {
+		return ClienteCalculadoraConsumer.callCalculadoraClienteObject(tipoIdentificacion, identificacion, tipoConsulta,
+				calificacion);
+	}
+	
+	/**
+	 * Metodo que se encarga de extraer la informacion de la Oferta desde el servicio Quski Clientes
+	 * @param perfilRiesgo
+	 * @param origenOperacion
+	 * @param riesgoTotal
+	 * @param fechaNacimiento
+	 * @param perfilPreferencia
+	 * @param agenciaOriginacion
+	 * @param identificacionCliente
+	 * @param calificacionMupi
+	 * @param coberturaExcepcionada
+	 * @param tipoJoya
+	 * @param descripcion
+	 * @param estadoJoya
+	 * @param tipoOroKilataje
+	 * @param pesoGr
+	 * @param tienePiedras
+	 * @param detallePiedras
+	 * @param descuentoPesoPiedras
+	 * @param pesoNeto
+	 * @param precioOro
+	 * @param valorAplicableCredito
+	 * @param valorRealizacion
+	 * @param numeroPiezas
+	 * @param descuentoSuelda
+	 * @returnObjeto con la informacion retornado con la oferta, o el error generado.
+	 * @author BRAYAN MONGE - Relative Engine
+	 * @throws RelativeException
+	 */
+
+
+	public SimularResponse getClienteInformacionOferta(final String perfilRiesgo, final String origenOperacion,
+			final String riesgoTotal,final  String fechaNacimiento,final  String perfilPreferencia, final String agenciaOriginacion,
+			final String identificacionCliente,final  String calificacionMupi, final String coberturaExcepcionada,final  String tipoJoya,
+			final String descripcion,final  String estadoJoya,final  String tipoOroKilataje, final String pesoGr,final  String tienePiedras,
+			final String detallePiedras,final  String descuentoPesoPiedras,final  String pesoNeto, final String precioOro,
+			final String valorAplicableCredito,final  String valorRealizacion,final  String numeroPiezas,final  String descuentoSuelda)
+			throws RelativeException {
+		return ClienteConsultaOfertaConsumer.callCalculadoraClienteObject(perfilRiesgo, origenOperacion, riesgoTotal, fechaNacimiento, perfilPreferencia, agenciaOriginacion, identificacionCliente, calificacionMupi, coberturaExcepcionada, tipoJoya, descripcion, estadoJoya, tipoOroKilataje, pesoGr, tienePiedras, detallePiedras, descuentoPesoPiedras, pesoNeto, precioOro, valorAplicableCredito, valorRealizacion, numeroPiezas, descuentoSuelda);
+	}
 }
-	
-	
-
-	
-
