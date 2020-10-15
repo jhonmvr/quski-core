c4ecede2d917e7809e81414fbb29bffcdf890896
developer-four <developer-four@relative-engine.com>
2020-10-15 16:23:19 -0500

    Cambios en los datamodel los estados no estan como enum

--- quski-oro-repository-ejb/src/main/java/com/relative/quski/repository/imp/PatrimonioRepositoryImp.java
+++ quski-oro-repository-ejb/src/main/java/com/relative/quski/repository/imp/PatrimonioRepositoryImp.java
@@ -1,13 +1,29 @@
 package com.relative.quski.repository.imp;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 
+import com.relative.core.exception.RelativeException;
 import com.relative.core.persistence.GeneralRepositoryImp;
+import com.relative.core.util.main.Constantes;
 import com.relative.quski.model.TbQoPatrimonio;
 import com.relative.quski.repository.PatrimonioRepository;
+import com.relative.quski.repository.spec.PatrimonioByIdClienteSpec;
 /**
  * Session Bean implementation class ParametrosRepositoryImp
  */
 @Stateless(mappedName = "patrimonioRepository")
 public class PatrimonioRepositoryImp extends GeneralRepositoryImp<Long, TbQoPatrimonio> implements PatrimonioRepository{
+
+	@Override
+	public List<TbQoPatrimonio> findByIdCliente(Long id) throws RelativeException {
+		try {
+			return findAllBySpecification(
+					new PatrimonioByIdClienteSpec( id ));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RelativeException(Constantes.ERROR_CODE_CUSTOM, "AL BUSCAR precios de oro por cotizador");
+		}
+	}
 }
