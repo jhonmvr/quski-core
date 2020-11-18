aec7425ee5d910941b1bb5afc9b2fcf7f639f0e7
Developer Twelve <developer-twelve@relative-engine.com>
2020-11-17 21:17:07 -0500

    Cambios de cuenta bancaria en el componente de generar credito

--- quski-oro-service-ejb/src/main/java/com/relative/quski/service/QuskiOroService.java
+++ quski-oro-service-ejb/src/main/java/com/relative/quski/service/QuskiOroService.java
@@ -5064,6 +5064,7 @@ public class QuskiOroService {
 					return op;
 				}
 			}
+			op.setCuentas( this.cuentaBancariaRepository.findByIdCliente( op.getCredito().getTbQoNegociacion().getTbQoCliente().getId() ) );
 			return op;
 		}catch(RelativeException e ){
 			throw new RelativeException(Constantes.ERROR_CODE_READ, QuskiOroConstantes.ERROR_AL_REALIZAR_BUSQUEDA + e.getLocalizedMessage());
