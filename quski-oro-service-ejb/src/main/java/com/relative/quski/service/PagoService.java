cc25454938f6406336751fe76f67beeb72b48202
Jhon Romero <developer-eleven@relative-engine.com>
2021-02-18 13:54:10 -0500

    fix web service object storage

--- quski-oro-service-ejb/src/main/java/com/relative/quski/service/PagoService.java
+++ quski-oro-service-ejb/src/main/java/com/relative/quski/service/PagoService.java
@@ -80,10 +80,9 @@ public class PagoService {
 				file.setName( e.getComprobante().getName() );
 				file.setProcess(EstadoEnum.ACT);
 				RespuestaObjectWrapper objeto;
-				String url = parametroRepository.findByNombre(QuskiOroConstantes.URL_STORAGE).getValor();
-				String base = parametroRepository.findByNombre(QuskiOroConstantes.DATA_BASE_NAME).getValor();
-				String coleccion = parametroRepository.findByNombre(QuskiOroConstantes.COLLECTION_NAME).getValor();
-				objeto = LocalStorageClient.createObject(url.concat("?databaseName=").concat(base).concat("&collectionName=").concat(coleccion),file, null );
+				objeto = LocalStorageClient.createObjectBig(parametroRepository.findByNombre(QuskiOroConstantes.URL_STORAGE).getValor(),
+						parametroRepository.findByNombre(QuskiOroConstantes.DATA_BASE_NAME).getValor(),
+						parametroRepository.findByNombre(QuskiOroConstantes.COLLECTION_NAME).getValor(),file, null );
 				TbQoRegistrarPago pago = new TbQoRegistrarPago();
 				pago.setIdComprobante(objeto.getEntidad());					
 				pago.setCuentas(e.getCuenta());
