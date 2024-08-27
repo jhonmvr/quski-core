6d512b7964ba1271a0a734d2cdb658b6e96e8a15
jhonromerore <jhon.romero@relative-engine.com>
2024-08-26 22:14:31 -0500

    fix pruebas 26/8

--- quski-oro-service-ejb/src/main/java/com/relative/quski/service/QuskiOroService.java
+++ quski-oro-service-ejb/src/main/java/com/relative/quski/service/QuskiOroService.java
@@ -6629,7 +6629,7 @@ public class QuskiOroService {
 						op, autorizacion, this.parametroRepository.findByNombre(QuskiOroConstantes.SOFTBANK_RENOVAR_OPERACION).getValor());
 				this.guardarOperacionRenovacion( operacion, wp.getCredito() );
 				CreditoCreadoSoftbank result = new CreditoCreadoSoftbank(  );
-				result.setCredito(credito);
+				//result.setCredito(credito); da error al parsear el json por que es muy grande
 				this.actualizarGarantiasSoftBank(credito.getTbQoTasacions(), wp.getCredito().getNumeroOperacionMadre(),wp.getCredito().getNumeroOperacionAnterior(), Boolean.FALSE, wp.getAsesor(), autorizacion);
 				result.setCuotasAmortizacion( this.consultarTablaAmortizacion( operacion.getNumeroOperacion(), operacion.getUriHabilitantes(),  op.getDatosRegistro(),autorizacion)  );
 				String sinExcepcion = "SIN EXCEPCION";
